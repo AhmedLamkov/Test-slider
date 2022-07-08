05e7cda29e89299618d520cfe2d9a979f98db9bd
Ahmed <ahmedik9809@gmail.com>
2022-07-08 23:42:56 +0300

    add responsiveness

--- src/Slider.js
+++ src/Slider.js
@@ -1,143 +1,169 @@
 export default class Slider {
-  config;
-  wrapper;
-  items;
-  itemWidth;
-  posX1 = 0;
-  posX2 = 0;
-  posInitial;
-  posFinal;
-  index = 0;
-  allowShift = true;
-  autoPlayInterval;
+	config;
+	wrapper;
+	items;
+	itemWidth;
+	posX1 = 0;
+	posX2 = 0;
+	posInitial;
+	posFinal;
+	index = 0;
+	allowShift = true;
+	autoPlayInterval;
 
-  constructor(config) {
-    this.config = config;
-    this.init();
-  }
+	constructor(config) {
+		this.config = config;
+		this.init();
+	}
 
-  init() {
-    this.initParams();
-    this.initListeners();
-    this.initInterval();
-  }
+	init() {
+		this.initParams();
+		this.initListeners();
+		this.initInterval();
+	}
 
-  initParams() {
-    this.body = document.querySelector(this.config.body);
-    this.wrapper = this.body.querySelector(this.config.wrapper);
-    this.items = this.body.querySelectorAll(this.config.item);
-    this.prevButton = document.querySelector(this.config.prevButton);
-    this.nextButton = document.querySelector(this.config.nextButton);
-    this.progress = document.querySelector(this.config.progress);
-    this.itemWidth = 300;
+	initParams() {
+		this.body = document.querySelector(this.config.body);
+		this.wrapper = this.body.querySelector(this.config.wrapper);
+		this.items = this.body.querySelectorAll(this.config.item);
+		this.prevButton = document.querySelector(this.config.prevButton);
+		this.nextButton = document.querySelector(this.config.nextButton);
+		this.progress = document.querySelector(this.config.progress);
+		this.checkBreakpoints();
 
-    this.wrapper.append(...Array.from(this.items).map(item => item.cloneNode(true)));
-    this.wrapper.prepend(...Array.from(this.items).map(item => item.cloneNode(true)));
-  }
+		this.wrapper.append(
+			...Array.from(this.items).map((item) => item.cloneNode(true))
+		);
+		this.wrapper.prepend(
+			...Array.from(this.items).map((item) => item.cloneNode(true))
+		);
+	}
 
-  initListeners() {
-    this.wrapper.onmousedown = (e) => this.dragStart(e);
-    
-    this.wrapper.addEventListener('touchstart', (e) => this.dragStart(e));
-    this.wrapper.addEventListener('touchend', (e) => this.dragEnd(e));
-    this.wrapper.addEventListener('touchmove', (e) => this.dragAction(e));
-    
-    this.prevButton.addEventListener('click', () => this.shiftSlide(-1));
-    this.nextButton.addEventListener('click', () => this.shiftSlide(1));
-    
-    this.wrapper.addEventListener('transitionend', () => this.checkIndex());
-  }
+	initListeners() {
+		this.wrapper.onmousedown = (e) => this.dragStart(e);
 
-  initInterval() {
-    this.progress.classList.add('active');
+		this.wrapper.addEventListener("touchstart", (e) => this.dragStart(e));
+		this.wrapper.addEventListener("touchend", (e) => this.dragEnd(e));
+		this.wrapper.addEventListener("touchmove", (e) => this.dragAction(e));
 
-    this.autoPlayInterval = setInterval(() => {
-      this.shiftSlide(1);
-    }, this.config.autoPlayInterval)
-  }
+		this.prevButton.addEventListener("click", () => this.shiftSlide(-1));
+		this.nextButton.addEventListener("click", () => this.shiftSlide(1));
 
-  resetInterval() {
-    clearInterval(this.autoPlayInterval);
-    this.progress.classList.remove('active');
-    this.progress.animation = 'none';
-    this.progress.offsetHeight;
-  }
+		this.wrapper.addEventListener("transitionend", () => this.checkIndex());
 
-  dragStart(e) {
-    e = e || window.event;
-    e.preventDefault();
-    this.posInitial = this.wrapper.offsetLeft;
-    this.resetInterval();
-    
-    if (e.type == 'touchstart') {
-      this.posX1 = e.touches[0].clientX;
-    } else {
-      this.posX1 = e.clientX;
-      document.onmouseup = (e) => this.dragEnd(e);
-      document.onmousemove = (e) => this.dragAction(e);
-    }
-  }
+		window.addEventListener("resize", () => this.checkBreakpoints());
+	}
 
-  dragAction(e) {
-    e = e || window.event;
-    
-    if (e.type == 'touchmove') {
-      this.posX2 = this.posX1 - e.touches[0].clientX;
-      this.posX1 = e.touches[0].clientX;
-    } else {
-      this.posX2 = this.posX1 - e.clientX;
-      this.posX1 = e.clientX;
-    }
-    this.wrapper.style.left = (this.wrapper.offsetLeft - this.posX2) + "px";
-  }
-  
-  dragEnd(e) {
-    this.posFinal = this.wrapper.offsetLeft;
-    if (this.posFinal - this.posInitial < -this.config.threshold) {
-      this.shiftSlide(1, 'drag');
-    } else if (this.posFinal - this.posInitial > this.config.threshold) {
-      this.shiftSlide(-1, 'drag');
-    } else {
-      this.initInterval();
-      this.wrapper.style.left = (this.posInitial) + "px";
-    }
+	initInterval() {
+		this.progress.classList.add("active");
 
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  
-  shiftSlide(dir, action) {
-    this.wrapper.classList.add('shifting');
-    if (this.allowShift) {
-      if (!action) { this.posInitial = this.wrapper.offsetLeft; }
+		this.autoPlayInterval = setInterval(() => {
+			this.shiftSlide(1);
+		}, this.config.autoPlayInterval);
+	}
 
-      if (dir == 1) {
-        this.wrapper.style.left = (this.posInitial - this.itemWidth) + "px";
-        this.index++;      
-      } else if (dir == -1) {
-        this.wrapper.style.left = (this.posInitial + this.itemWidth) + "px";
-        this.index--;      
-      }
-    };
-    this.resetInterval();
-    this.initInterval();
-    
-    this.allowShift = false;
-  }
-    
-  checkIndex() {
-    this.wrapper.classList.remove('shifting');
-    const itemsLength = this.items.length;
-    if (this.index == -1) {
-      this.wrapper.style.left = -(itemsLength * this.itemWidth) + "px";
-      this.index = itemsLength - 1;
-    }
+	resetInterval() {
+		clearInterval(this.autoPlayInterval);
+		this.progress.classList.remove("active");
+		this.progress.animation = "none";
+		this.progress.offsetHeight;
+	}
 
-    if (this.index == itemsLength) {
-      this.wrapper.style.left = -(1 * this.itemWidth) + "px";
-      this.index = 0;
-    }
-    
-    this.allowShift = true;
-  }
+	dragStart(e) {
+		e = e || window.event;
+		e.preventDefault();
+		this.posInitial = this.wrapper.offsetLeft;
+		this.resetInterval();
+
+		if (e.type == "touchstart") {
+			this.posX1 = e.touches[0].clientX;
+		} else {
+			this.posX1 = e.clientX;
+			document.onmouseup = (e) => this.dragEnd(e);
+			document.onmousemove = (e) => this.dragAction(e);
+		}
+	}
+
+	dragAction(e) {
+		e = e || window.event;
+
+		if (e.type == "touchmove") {
+			this.posX2 = this.posX1 - e.touches[0].clientX;
+			this.posX1 = e.touches[0].clientX;
+		} else {
+			this.posX2 = this.posX1 - e.clientX;
+			this.posX1 = e.clientX;
+		}
+		this.wrapper.style.left = this.wrapper.offsetLeft - this.posX2 + "px";
+	}
+
+	dragEnd(e) {
+		this.posFinal = this.wrapper.offsetLeft;
+		if (this.posFinal - this.posInitial < -this.config.threshold) {
+			this.shiftSlide(1, "drag");
+		} else if (this.posFinal - this.posInitial > this.config.threshold) {
+			this.shiftSlide(-1, "drag");
+		} else {
+			this.initInterval();
+			this.wrapper.style.left = this.posInitial + "px";
+		}
+
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+
+	shiftSlide(dir, action) {
+		this.wrapper.classList.add("shifting");
+		if (this.allowShift) {
+			if (!action) {
+				this.posInitial = this.wrapper.offsetLeft;
+			}
+			if (dir == 1) {
+				this.wrapper.style.left = this.posInitial - this.itemWidth + "px";
+				this.index++;
+			} else if (dir == -1) {
+				this.wrapper.style.left = this.posInitial + this.itemWidth + "px";
+				this.index--;
+			}
+		}
+		this.resetInterval();
+		this.initInterval();
+
+		this.allowShift = false;
+	}
+
+	checkIndex() {
+		this.wrapper.classList.remove("shifting");
+		const itemsLength = this.items.length;
+		if (this.index == -1) {
+			this.wrapper.style.left = -(itemsLength * this.itemWidth) + "px";
+			this.index = itemsLength - 1;
+		}
+
+		if (this.index == itemsLength) {
+			this.wrapper.style.left = -(1 * this.itemWidth) + "px";
+			this.index = 0;
+		}
+
+		this.allowShift = true;
+	}
+
+	checkBreakpoints() {
+		const oldWidth = this.itemWidth;
+		let hasChanged;
+		Object.keys(this.config.breakpoints).forEach((key) => {
+			if (parseInt(key) >= window.innerWidth) {
+				const breakpoint = this.config.breakpoints[key];
+				this.itemWidth = breakpoint.itemWidth;
+				this.threshold = breakpoint.threshold;
+				hasChanged = true;
+			}
+		});
+
+		if (!hasChanged) {
+			this.itemWidth = this.config.itemWidth;
+		}
+
+		this.wrapper.style.left = -(this.itemWidth * this.index) + "px";
+	}
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import("./scss/main.scss");
 import Slider from "./Slider";
 
 const slider = new Slider({
-  body: '.slider__body',
-  wrapper: '.slider__body-wrapper',
-  item: '.slider__item',
-  spaceBetween: 40,
-  threshold: 100,
-  prevButton: '#previous',
-  nextButton: '#next',
-  progress: '#progress-completed',
-  autoPlayInterval: 4000,
-})
+	body: ".slider__body",
+	wrapper: ".slider__body-wrapper",
+	item: ".slider__item",
+	spaceBetween: 40,
+	itemWidth: 300,
+	threshold: 100,
+	prevButton: "#previous",
+	nextButton: "#next",
+	progress: "#progress-completed",
+	autoPlayInterval: 4000,
+	breakpoints: {
+		576: {
+			itemWidth: 260,
+			threshold: 40,
+		},
+	},
+});
